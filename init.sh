f239db108689caead0407a2241c260bcb509db22
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-26 20:34:38 -0300

    chore: remove unecessary env from init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -d --network pfa-docker --name pfa-docker-mysql -e MYSQL_ROOT_PASSWORD=root devontherun/pfa-docker-mysql
+docker run --rm -d --network pfa-docker --name pfa-docker-mysql devontherun/pfa-docker-mysql
 docker run --rm -d --network pfa-docker --name pfa-docker-node devontherun/pfa-docker-node
 docker run --rm -d --network pfa-docker -p 8080:80 --name pfa-docker-nginx devontherun/pfa-docker-nginx
